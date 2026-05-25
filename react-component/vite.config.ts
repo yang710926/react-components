922290242165e98798079b17ca72063e1c302f12
yangjiacong <2584888303@qq.com>
2026-05-25 19:26:30 +0800

    Configure for GitHub Pages deployment

--- react-component/vite.config.ts
+++ react-component/vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-component/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
